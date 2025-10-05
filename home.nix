db38419571c6e0d4d45beae686df1d2c3473261e
SychosMILEz <gabeforemana94@gmail.com>
2025-10-05 12:25:52 -0700

    Auto update: rebuild on (date +%Y-%m-%d_%H:%M:%S)

--- home.nix
+++ home.nix
@@ -103,7 +103,7 @@
   # 🧰 Waybar / Hyprland extras (if you have custom configs)
   xdg.configFile."waybar/config.jsonc".source = ./waybar/config.jsonc;
   xdg.configFile."waybar/style.css".source = ./waybar/style.css;
-  xdg.configFile."hypr/hyprland.conf".source = ./hypr/hyprland.conf;
+  xdg.configFile."hypr".source = ./hypr;
   
    home.stateVersion = "25.05";
 }
